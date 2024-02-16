068d41e0ae8202e192591ac32b5a664a76ccf556
George Cave <cave@hey.com>
2024-02-16 16:15:59 +0000

    Add PS2Keyboard library

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,49 @@
-/*  PS2Keyboard library example
-  
-  PS2Keyboard now requries both pins specified for begin()
-
-  keyboard.begin(data_pin, irq_pin);
-  
-  Valid irq pins:
-     Arduino Uno:  2, 3
-     Arduino Due:  All pins, except 13 (LED)
-     Arduino Mega: 2, 3, 18, 19, 20, 21
-     Teensy 2.0:   All pins, except 13 (LED)
-     Teensy 2.0:   5, 6, 7, 8
-     Teensy 1.0:   0, 1, 2, 3, 4, 6, 7, 16
-     Teensy++ 2.0: 0, 1, 2, 3, 18, 19, 36, 37
-     Teensy++ 1.0: 0, 1, 2, 3, 18, 19, 36, 37
-     Sanguino:     2, 10, 11
-  
-  for more information you can read the original wiki in arduino.cc
-  at http://www.arduino.cc/playground/Main/PS2Keyboard
-  or http://www.pjrc.com/teensy/td_libs_PS2Keyboard.html
-  
-  Like the Original library and example this is under LGPL license.
-  
-  Modified by Cuninganreset@gmail.com on 2010-03-22
-  Modified by Paul Stoffregen <paul@pjrc.com> June 2010
-*/
+/* Baby Keyboard */
    
 #include <Arduino.h>
 #include <PS2Keyboard.h>
 
-const int DataPin = 2;
-const int IRQpin =  3;
+const int pin_data = 2;
+const int pin_IRQ =  3;
 
 PS2Keyboard keyboard;
 
+uint8_t baby_letter = 0;
+uint8_t num_aaas = 1;
+uint8_t letter_a = 0;
+
+void speak_baby();
+
 void setup() {
   delay(1000);
-  keyboard.begin(DataPin, IRQpin);
-  Serial.begin(9600);
-  Serial.println("Keyboard Test:");
+  keyboard.begin(pin_data, pin_IRQ);
+  Serial.begin(115200);
 }
 
 void loop() {
-  if (keyboard.available()) {
-    
-    // read the next key
-    char c = keyboard.read();
-    
-    // check for some of the special keys
-    if (c == PS2_ENTER) {
-      Serial.println();
-    } else if (c == PS2_TAB) {
-      Serial.print("[Tab]");
-    } else if (c == PS2_ESC) {
-      Serial.print("[ESC]");
-    } else if (c == PS2_PAGEDOWN) {
-      Serial.print("[PgDn]");
-    } else if (c == PS2_PAGEUP) {
-      Serial.print("[PgUp]");
-    } else if (c == PS2_LEFTARROW) {
-      Serial.print("[Left]");
-    } else if (c == PS2_RIGHTARROW) {
-      Serial.print("[Right]");
-    } else if (c == PS2_UPARROW) {
-      Serial.print("[Up]");
-    } else if (c == PS2_DOWNARROW) {
-      Serial.print("[Down]");
-    } else if (c == PS2_DELETE) {
-      Serial.print("[Del]");
-    } else {
-      
-      // otherwise, just print all normal characters
-      Serial.print(c);
-    }
-  }
+	if (keyboard.available()) {
+		char c = keyboard.read();
+		speak_baby();
+  	}
+}
+
+void speak_baby(){
+	switch(baby_letter){
+		case 0:
+			Serial.print('w');
+			baby_letter++;
+			num_aaas = random(3, 10);
+			break;
+		case 1:
+			Serial.print('a');
+			letter_a++;
+			if(letter_a >= num_aaas){
+				baby_letter++;
+			}
+			break;
+		case 2:
+			Serial.print(' ');
+			baby_letter = 0;
+			break;
+	}
 }

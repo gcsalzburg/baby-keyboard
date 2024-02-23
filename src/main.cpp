50df224a3e83fb02f24ffea05da18049365e1b4b
George Cave <cave@hey.com>
2024-02-23 14:39:22 +0000

    Improve baby speak

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,33 @@
 /* Baby Keyboard */
    
 #include <Arduino.h>
+#include <Keyboard.h>
 #include <PS2Keyboard.h>
 
 const int pin_data = 2;
 const int pin_IRQ =  3;
 
-PS2Keyboard keyboard;
+PS2Keyboard ps2keyboard;
 
 uint8_t baby_letter = 0;
 uint8_t num_aaas = 1;
 
-const uint8_t min_a = 2;
-const uint8_t max_a = 10;
+const uint8_t min_a = 1;
+const uint8_t max_a = 7;
 uint8_t letter_a = 0;
 
 void speak_baby();
 
 void setup() {
   delay(1000);
-  keyboard.begin(pin_data, pin_IRQ);
-  Serial.begin(115200);
+  ps2keyboard.begin(pin_data, pin_IRQ);
+
+	Keyboard.begin();
 }
 
 void loop() {
-	if (keyboard.available()) {
-		char c = keyboard.read();
+	if (ps2keyboard.available()) {
+		ps2keyboard.read();
 		speak_baby();
   	}
 }
@@ -33,19 +35,20 @@ void loop() {
 void speak_baby(){
 	switch(baby_letter){
 		case 0:
-			Serial.print('w');
+			Keyboard.write('w');
 			baby_letter++;
 			num_aaas = random(min_a, max_a);
+			letter_a = 0;
 			break;
 		case 1:
-			Serial.print('a');
+			Keyboard.write('a');
 			letter_a++;
 			if(letter_a >= num_aaas){
 				baby_letter++;
 			}
 			break;
 		case 2:
-			Serial.print(' ');
+			Keyboard.write(' ');
 			baby_letter = 0;
 			break;
 	}
